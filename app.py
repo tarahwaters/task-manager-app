b72e22620b388f4987ef19b9144973ab7a61a7c1
tarahwaters <thbwaters@outlook.com>
2023-08-23 15:12:24 +0000

    Import additional functionalities from Flask at the top of app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
